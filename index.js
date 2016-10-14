02fb2dd152dfd31a22fd030ca5a19a79e3383168
Aya Shameimaru <noscoper4ver@gmail.com>
2016-10-14 07:25:29 -0700

    ror

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+const Client = require("./code/Client");
+
+function DHTC(token) {
+	return new Client(token)
+	/* discord.htc Client Passer */
+}
+
+const Message = require("./code/structs/Message");
+const User = require("./code/structs/User");
+const Channel = require("./code/structs/Channel");
+const Guild = require("./code/structs/Guild");
+const VoiceRegion = require("./code/structs/voice/VoiceRegion");
+const VoiceServer = require("./code/structs/voice/VoiceServer");
+const Role = require("./code/structs/Role");
+const Invite = require("./code/structs/Invite");
+
+DHTC.Client = Client;
+DHTC.Message = Message;
+DHTC.User = User;
+DHTC.Channel = Channel;
+DHTC.Guild = Guild;
+DHTC.VoiceRegion = VoiceRegion;
+DHTC.VoiceServer = VoiceServer;
+DHTC.Role = Role;
+DHTC.Invite = Invite;
+
+module.exports = DHTC;
